30cfeefd1e2230bcd3447f536d8b7e6c30268e8e
zhenga12 <zhenga12@mcmaster.ca>
2024-02-16 19:57:51 -0500

    Add tools used for contrast enhancement
    grey_image.py   -- creates greyscale image based on jpg in folder
                       also performs contrast enhancement
                       will save before/after images to be used for comparision
    
    hex_to_image.py -- reads hex values dumped into .txt file (copied from log file) to recreate image
                       compare this result with result from python script

---
+++ debug_tools/contrast_enhance/grey_image.py
@@ -0,0 +1,62 @@
+'''
+Generates greyscale image and performs contrast enhancement
+'''
+import cv2 as cv
+import numpy
+
+IMAGE_PATH = "roof-balloons.jpg"
+numpy.set_printoptions(threshold=numpy.inf)
+SCALE_FACTOR = 32 #no issues on simulation when set to 64
+ENHANCEMENT_MASK = [[0, -1, 0],
+                    [-1, 5, -1],
+                    [0, -1, 0]]
+WRITE_TO_FILE = 1
+
+def to_text(img, filename="default_name.txt", format=None):
+    txt_file = open(filename, 'w')
+    txt_file.write("{")
+    #row = img.shape[0]
+    #col = img.shape[1]
+    for row in range(img.shape[0]):
+        txt_file.write("{")
+        for col in range(img.shape[1]):
+            if format == "hex":
+                txt_file.write(str(hex(img[row][col]))+", ")
+            else:
+                txt_file.write(str(img[row][col])+", ")
+        txt_file.write("},")
+        txt_file.write("\n")
+    txt_file.write("}")
+    #txt_file.write(numpy.array2string(img))
+    txt_file.close()
+
+base_img = cv.imread(IMAGE_PATH)
+greyscale = cv.cvtColor(base_img, cv.COLOR_BGR2GRAY)
+base_width = greyscale.shape[0]
+base_height = greyscale.shape[1]
+new_dim = (int(base_height/SCALE_FACTOR), int(base_width/SCALE_FACTOR))
+#print(f"{base_width} x {base_height}")
+greyscale = cv.resize(greyscale, new_dim)
+base_width = greyscale.shape[0]
+base_height = greyscale.shape[1]
+print(f"{base_width} x {base_height}")
+
+cv.imwrite("greyscale_image.png", greyscale)
+
+enhanced = numpy.zeros((base_width, base_height),dtype=(numpy.int32))
+#enhanced = numpy.zeros((base_width, base_height))
+
+for row in range(1,base_width-1):
+    for col in range(1,base_height-1):
+        new_val = numpy.vdot(greyscale[row-1:row+2,col-1:col+2], 
+                             ENHANCEMENT_MASK)
+        if new_val < 0: new_val = 0
+        enhanced[row][col] = new_val
+
+cv.imwrite("greyscale_enhance.png", enhanced)
+
+if WRITE_TO_FILE:
+    to_text(greyscale, "greyscale.txt")
+    to_text(enhanced, "greyscale_enhanced.txt")
+    to_text(enhanced, "greyscale_enhanced_hex.txt", "hex")
+
---
+++ debug_tools/contrast_enhance/hex_to_image.py
@@ -0,0 +1,36 @@
+'''
+read hex values dumped by simulation log
+can use to compare simulated values with values produced the python script
+'''
+import numpy
+import cv2
+
+#adjust dimensions based on dimensions of image
+#can see issues when 94x126 image used
+WIDTH  = 47
+HEIGHT = 63
+input_file = open("fpga_output.txt", "r")
+
+image = [];
+text_data = input_file.read().splitlines()
+#print(type(text_data[0:10].splitlines()))
+
+for line in text_data:
+    #print(str(text_data[127]))
+    
+    #thing = line.strip()
+    #print(line.split(" "))
+    for val in line.split(" "):
+        #print(val)
+        #int(val.strip(),16)
+        if val == '':
+            continue
+        image.append(int(val.strip(),16))
+
+input_file.close()
+
+image = numpy.array(image)
+
+image = image.reshape((HEIGHT, WIDTH))
+#print(image[0])
+cv2.imwrite("recreated_img_grey.png", image)
